0d4d6cceb508145b684af17966010bb2622929d4
Darkstar <darkstar>
2020-06-18 02:37:49 -0700

    Minimize Debug info

--- device.mk
+++ device.mk
@@ -87,9 +87,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AdvancedDisplay
 	
-#PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-#PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false	
-	
 # APEX
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
