588a956f6c853b6e872efa68502355701958defd
Prabhdeep Singh <67806882+AospRemixer@users.noreply.github.com>
2020-09-30 16:45:00 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ PRODUCT_CHARACTERISTICS := tablet
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-arrow
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
